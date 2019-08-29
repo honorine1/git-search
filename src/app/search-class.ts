0d563664e9745c6aa1eda985b9801c3f8c13bac2
honorine1 <uwurukundoh@gmail.com>
2019-08-29 18:45:47 +0200

    HTTP service that uses a promise to make the requests to the GitHub API. is added

--- src/app/search-class.ts
+++ src/app/search-class.ts
@@ -1,3 +1,3 @@
 export class SearchClass {
-    constructor(public login:any,public public_repos:any, public avatar_url:any){}
+    constructor(public login:any,public public_repos:any, public avatar_url:any, public name:string, public html_url:any){}
 }
--- src/app/search-http/search-request.service.ts
+++ src/app/search-http/search-request.service.ts
@@ -12,25 +12,32 @@ export class SearchRequestService {
   searchClass:SearchClass;
 
   constructor(private http:HttpClient){
-this.searchClass = new SearchClass("","","")
+this.searchClass = new SearchClass("","","","","")
   } 
   searchRequest(){
     interface ApiResponse{
       login:any;
       public_repos:any;
       avatar_url:any;
+      name:string;
+      html_url:any;      
+
     }
     let promise = new Promise((resolve,reject)=>{
       this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
         this.searchClass.login = response.login
         this.searchClass.public_repos = response.public_repos
         this.searchClass.avatar_url = response.avatar_url
+        this.searchClass.name = response.name
+        this.searchClass.html_url = response.html_url
         resolve()
       }),
       error=>{
         this.searchClass.login= 'no username'
         this.searchClass.public_repos = 'no repos number'
         this.searchClass.avatar_url= 'profile is not showing up'
+        this.searchClass.name= 'you did not have a github name'
+        this.searchClass.html_url= 'dont you have a github link?'
         reject(error)
   }
  
