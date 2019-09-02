f26d36bd705a0e8d81c1fb532315e5dc79fc1bd8
honorine1 <uwurukundoh@gmail.com>
2019-09-02 17:30:43 +0200

    profile css is added

--- src/app/search-class.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchClass } from './search-class';
-
-describe('SearchClass', () => {
-  it('should create an instance', () => {
-    // expect(new SearchClass()).toBeTruthy();
- 
-  });
-});
--- src/app/search-class.ts
+++
@@ -1,4 +0,0 @@
-export class SearchClass {
-    constructor(public login:any,public public_repos:any, public avatar_url:any, public name:string, 
-        public html_url:any,public following:any, public followers:any){}
-}
